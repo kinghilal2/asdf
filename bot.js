0b4a844248599c3347f363cd6b5b62d323197aa1
kinghilal2 <47920420+kinghilal2@users.noreply.github.com>
2019-03-08 22:20:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,7 +274,7 @@ message.channel.send(embed)
 
 
 client.on('message', async message => {
-    let amount = 25000000000000000000;
+    let amount = NaN;
     if(message.content.startsWith(prefix + "1")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
